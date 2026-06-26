26a342a458284b0c44f4677f7144958f9c491eaa
Thamsanqa Hadebe <thamsanqahadebe98@gmail.com>
2026-06-26 11:45:12 +0200

    refactor: implemented refactored, correct logic in BinaryNumberGenerator, within the generate() method.

--- src/main/java/com/assessment/queue/BinaryNumberGenerator.java
+++ src/main/java/com/assessment/queue/BinaryNumberGenerator.java
@@ -41,14 +41,19 @@ public class BinaryNumberGenerator {
     public List<String> generate(int n) {
         // TODO
         if (n < 1) throw new IllegalArgumentException();
-        Queue<String> queue = new LinkedList<String>();
-        List<String> result = new ArrayList<String>();
+        Queue<String> queue = new LinkedList<>();
+        List<String> result = new ArrayList<>();
 
-        String str = String.valueOf(n);
-        queue.offer(str);
-        for (String item : queue) {
-            result.add(item);
+        queue.offer("1");
+
+        for (int i = 0; i < n; i++) {
+            String current = queue.poll();
+            result.add(current);
+
+            queue.offer(current + "0");
+            queue.offer(current + "1");
         }
+
         return result;
     }
 }
